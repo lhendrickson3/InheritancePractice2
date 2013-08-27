deb91720bde2c4148e8ee2d4ea4d2e79951e45f5
lhendrickson3@my.wctc.edu <>
2013-08-27 16:28:07 -0500

    added content and comments still working on myconcrete... for some reason it has an error when I try to run it with new salariedemployee mySalariedEmployee

--- src/myconcrete/Runner.java
+++ src/myconcrete/Runner.java
@@ -10,5 +10,30 @@ package myconcrete;
  * @author User
  */
 public class Runner {
-    
+
+  public static void main(String[] args){
+  //Calling the methods from HourlyEmployee
+  HourlyEmployee myEmployee = new HourlyEmployee();
+  myEmployee.HowManyHoursWorked();
+  myEmployee.PayPerHour();
+  myEmployee.setPay();
+  myEmployee.setAmountPaid(myEmployee.getPay());
+      System.out.println("Amount paid " + myEmployee.getAmountPaid());
+  //Calling the methods from SalariedEmployee
+  SalariedEmployee mySalariedEmployee = new SalariedEmployee();
+  mySalariedEmployee.HowManyYearsWorked();
+  mySalariedEmployee.YearlySalary();
+  mySalariedEmployee.setSalaryPay();
+  mySalariedEmployee.setAmountPaid(mySalariedEmployee.getSalaryPay());
+  System.out.println("Salary paid " + mySalariedEmployee.getSalaryPay());
+  //Calling the methods for SalaryPlusBonusEmployee
+  SalaryPlusBonusEmployee myBonusEmployee = new SalaryPlusBonusEmployee();
+  myBonusEmployee.HowManyYearsWorked();
+  myBonusEmployee.YearlySalary();
+  myBonusEmployee.BonusPay();
+  myBonusEmployee.setSalaryPay();
+  myBonusEmployee.setTotalPay();
+  System.out.println("Total salary (plus bonus) paid " + myBonusEmployee.getTotalPay());
+  
+  }
 }
