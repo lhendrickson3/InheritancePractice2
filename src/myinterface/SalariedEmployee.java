0fbbab295c2280a10d4d77083085e0924c579008
lhendrickson3@my.wctc.edu <>
2013-08-27 16:06:59 -0500

    Created 3 packages each with Employee, HourlyEmployee, SalariedEmployee, and SalaryPlusBonusEmployee

---
+++ src/myinterface/SalariedEmployee.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myinterface;
+
+
+/**
+ *
+ * @author User
+ */
+public class SalariedEmployee {
+    
+}
